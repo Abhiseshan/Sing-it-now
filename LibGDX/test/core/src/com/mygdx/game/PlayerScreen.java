15f65e0b3f8a4d72252033b5fa53fee16c085e07
Abhinav Rajaseshan <abhinavseshan@hotmail.com>
2015-09-19 11:22:48 -0400

    new

--- LibGDX/test/core/src/com/mygdx/game/PlayerScreen.java
+++ LibGDX/test/core/src/com/mygdx/game/PlayerScreen.java
@@ -23,9 +23,6 @@ import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.nio.file.Paths;
 
-/**
- * Created by Abhinav on 9/19/2015.
- */
 public class PlayerScreen implements Screen{
 
     SpriteBatch batch;
--- LibGDX/test/core/src/com/mygdx/game/SelectionScreen.java
+++ LibGDX/test/core/src/com/mygdx/game/SelectionScreen.java
@@ -8,6 +8,14 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 /**
  * Created by Abhinav on 9/19/2015.
  */
@@ -35,6 +43,58 @@ public class SelectionScreen implements Screen{
         artist = new BitmapFont();
         name = new BitmapFont();
         album = new BitmapFont();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                receiver();
+            }
+        });
+
+        thread.start();
+    }
+
+    private void receiver(){
+        DatagramSocket serverSocket = null;
+        try {
+            serverSocket = new DatagramSocket(50005);
+        } catch (SocketException e){
+            e.printStackTrace();
+        }
+
+        ServerSocket echoServer = null;
+        String line;
+        DataInputStream is;
+        PrintStream os;
+        Socket clientSocket = null;
+
+// Try to open a server socket on port 9999
+        try {
+            echoServer = new ServerSocket(9999);
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+// Create a socket object from the ServerSocket to listen and accept
+// connections.
+// Open input and output streams
+
+        try {
+            clientSocket = echoServer.accept();
+            is = new DataInputStream(clientSocket.getInputStream());
+            os = new PrintStream(clientSocket.getOutputStream());
+
+// As long as we receive data, echo that data back to the client.
+
+            while (true) {
+                line = is.readLine();
+                os.println(line);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
 
     @Override
